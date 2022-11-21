719f090687fd9d622f4b237ad1956697308ae1cc
Annel Gonzales <96323677+Annel702@users.noreply.github.com>
2022-11-21 10:53:09 -0800

    Add files via upload

---
+++ SCRUMDINGER/SCRUMDINGER/MeetingView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SCRUMDINGER
+//
+//  Created by Annel Gonzales on 11/17/22.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        ProgessView()
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
---
+++ SCRUMDINGER/SCRUMDINGER/SCRUMDINGERApp.swift
@@ -0,0 +1,17 @@
+//
+//  SCRUMDINGERApp.swift
+//  SCRUMDINGER
+//
+//  Created by Annel Gonzales on 11/17/22.
+//
+
+import SwiftUI
+
+@main
+struct SCRUMDINGERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
